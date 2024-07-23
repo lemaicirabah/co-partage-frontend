68af4ce7e71435a2a57deb8b4b09e3bee702090e
Carl-Montpetit <montpetit.carl@icloud.com>
2024-07-23 19:35:16 -0400

    Updated src/main.ts with router and vuetify imports

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+const app = createApp(App);
 
-createApp(App).use(vuetify).mount("#app");
+app.use(router);
+app.use(vuetify);
+
+app.mount("#app");
