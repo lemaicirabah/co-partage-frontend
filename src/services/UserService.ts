728d4808fa332581782c061ec4402d3a4e16de61
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-07-30 17:28:17 -0400

    refactor(UserService): update UserService to use class structure and consolidate API URL
    - Replaced object export with class structure for UserService
    - Consolidated API URL into a constant for easier maintenance
    - Added methods for creating, updating, and deleting users
    - Updated getUserDetails method to getUserById for consistency

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,10 +1,27 @@
-import axios from "@/plugins/axios";
+import axios from "axios";
 
-export default {
+const API_URL = "http://localhost:8080/co-partage/users";
+
+class UserService {
   getAllUsers() {
-    return axios.get("co-partage/users");
-  },
-  getUserDetails(id: string) {
-    return axios.get(`co-partage/users/${id}`);
-  },
-};
+    return axios.get(API_URL);
+  }
+
+  getUserById(id: number) {
+    return axios.get(`${API_URL}/${id}`);
+  }
+
+  createUser(user: any) {
+    return axios.post(API_URL, user);
+  }
+
+  updateUser(id: number, user: any) {
+    return axios.put(`${API_URL}/${id}`, user);
+  }
+
+  deleteUser(id: number) {
+    return axios.delete(`${API_URL}/${id}`);
+  }
+}
+
+export default new UserService();
