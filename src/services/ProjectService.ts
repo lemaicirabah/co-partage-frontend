d8f72ea4dd4971ebd55d1a3c85f013a2284c0f49
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-07-31 21:50:43 -0400

    Update ProjectService to use number for projectId and correct CRUD operations

--- src/services/ProjectService.ts
+++ src/services/ProjectService.ts
@@ -4,36 +4,43 @@ export default {
   getAllProjects() {
     return axios.get("co-partage/projects");
   },
+  getProjectById(id: number) {
+    return axios.get(`co-partage/projects/${id}`);
+  },
   getProjectDetails(id: string) {
     return axios.get(`co-partage/projects/${id}`);
   },
-  updateProject(id:string, projectData:string) {
+  updateProject(id: string, projectData: string) {
     return axios.put(`co-partage/projects/${id}`, projectData);
   },
-  goToTasks(projectId:string) {
+  goToTasks(projectId: string) {
     return axios.post(`co-partage/projects/${projectId}/tasks`);
   },
-  getTask(projectId:string, taskId:string) {
+  getTask(projectId: string, taskId: string) {
     return axios.get(`co-partage/projects/${projectId}/tasks/${taskId}`);
   },
-  createTask(projectId:string, taskData:string) {
+  createTask(projectId: string, taskData: string) {
     return axios.post(`co-partage/projects/${projectId}/tasks`, taskData);
   },
-  updateTask(projectId:string, taskId:string, taskData:string) {
-    return axios.put(`co-partage/projects/${projectId}/tasks/${taskId}`, taskData);
+  updateTask(projectId: string, taskId: string, taskData: string) {
+    return axios.put(
+      `co-partage/projects/${projectId}/tasks/${taskId}`,
+      taskData
+    );
   },
-  deleteTask(projectId:string, taskId:string) {
+  deleteTask(projectId: string, taskId: string) {
     return axios.delete(`co-partage/projects/${projectId}/tasks/${taskId}`);
   },
-  goToMembers(projectId:string) {
-    return axios.post(`co-partage/projects/${projectId}/members`);},
-  addMember(projectId:string, memberId:string) {
+  goToMembers(projectId: string) {
+    return axios.post(`co-partage/projects/${projectId}/members`);
+  },
+  addMember(projectId: string, memberId: string) {
     return axios.post(`co-partage/projects/${projectId}/members/${memberId}`);
   },
-    getMember(projectId:string, memberId:string) {
-      return axios.get(`co-partage/projects/${projectId}/members/${memberId}`);
-    },  
-  deleteMember(projectId:string, memberId:string) {
+  getMember(projectId: string, memberId: string) {
+    return axios.get(`co-partage/projects/${projectId}/members/${memberId}`);
+  },
+  deleteMember(projectId: string, memberId: string) {
     return axios.delete(`co-partage/projects/${projectId}/members/${memberId}`);
-  }
+  },
 };
