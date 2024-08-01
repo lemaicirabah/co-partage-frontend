578ba2d92addfc2b8062af7019681bb8c6a4d065
Carl Montpetit <montpetit.carl@courrier.uqam.ca>
2024-08-01 16:03:41 -0400

    Update router index.ts: Add Dashboard route, improve route formatting, ensure consistent import styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,15 +8,16 @@ import Register from "@/pages/Register.vue";
 import AllProjects from "@/components/AllProjects.vue";
 import ProjectDetails from "@/components/ProjectDetails.vue";
 import UpdateProject from "@/components/UpdateProject.vue";
-import ProjectTasks from '@/components/ProjectTasks.vue';
-import ProjectMembers from '@/components/ProjectMembers.vue';
-import AddMember from '@/components/AddMember.vue';
-import AddTask from '@/components/AddTask.vue';
-import EditTask from '@/components/EditTask.vue';
+import ProjectTasks from "@/components/ProjectTasks.vue";
+import ProjectMembers from "@/components/ProjectMembers.vue";
+import AddMember from "@/components/AddMember.vue";
+import AddTask from "@/components/AddTask.vue";
+import EditTask from "@/components/EditTask.vue";
 import AllUsers from "@/components/AllUsers.vue";
 import UserDetails from "@/components/UserDetails.vue";
 import AllEvaluations from "@/components/AllEvaluations.vue";
 import EvaluationDetails from "@/components/EvaluationDetails.vue";
+import Dashboard from "@/pages/Dashboard.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
@@ -26,14 +27,21 @@ const routes = [
     component: Projects,
     children: [
       { path: "all", name: "AllProjects", component: AllProjects },
-      { path: "details/:id", name: "ProjectDetails", component: ProjectDetails },
+      {
+        path: "details/:id",
+        name: "ProjectDetails",
+        component: ProjectDetails,
+      },
       { path: "update", name: "UpdateProject", component: UpdateProject },
       { path: ":id/tasks", name: "ProjectTasks", component: ProjectTasks },
-      { path: ":id/members", name: "ProjectMembers", component: ProjectMembers },
+      {
+        path: ":id/members",
+        name: "ProjectMembers",
+        component: ProjectMembers,
+      },
       { path: ":id/tasks/new", name: "AddTask", component: AddTask },
       { path: ":id/tasks/:taskId/edit", name: "EditTask", component: EditTask },
       { path: ":id/members/add", name: "AddMember", component: AddMember },
-      
     ],
   },
   {
@@ -61,6 +69,7 @@ const routes = [
   },
   { path: "/login", name: "Login", component: Login },
   { path: "/register", name: "Register", component: Register },
+  { path: "/dashboard", name: "Dashboard", component: Dashboard },
 ];
 
 const router = createRouter({
